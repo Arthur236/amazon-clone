7ab4efee718c718b151e8cc945be18ea6db7fcc4
Arthur236 <arthur.thungu@gmail.com>
2020-09-16 21:39:44 +0300

    initial setup

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./Header";
+
+import "./css/app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
